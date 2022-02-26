b786fe623aa2f86cf2d96e57f877b44b4de2a8cd
EnriqueMoranjobs <e.moran.l90@gmail.com>
2022-02-26 13:00:28 +0100

    First Commit, Login ended

---
+++ app/src/main/java/com/example/metalshop/Login.java
@@ -0,0 +1,49 @@
+package com.example.metalshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+//    Glide for loading girls
+        ImageView mHand = findViewById(R.id.fondohand);
+
+        Glide.with(this)
+                .load(R.drawable.hand)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+
+                .into(mHand);
+
+
+    }
+
+
+    // Method for Vaccinate button
+    public void openMain(View v) {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    public void openAdmin(View v) {
+        Intent intent = new Intent(Login.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
+
